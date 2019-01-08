4c539dfbeba58c924383d9f601529a0931815397
Steven Combs <scombs@MacBook-Pro-6.local>
2019-01-08 14:58:06 -0600

    cleaned up the app.py a bit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from chalice import Chalice
 import json
 
-app = Chalice(app_name='helloworld')
+app = Chalice(app_name='fizzbuzzcalculator')
 
 
 @app.route('/')
@@ -17,7 +17,6 @@ def fizzBuzzCalc():
     return { 'result' : fizzbuzzCalculator(int(app.current_request.json_body['request'])) }
 
 def fizzbuzzCalculator(n):
-
     if n == 0:
         return str(n)
     elif n % 3 == 0 and n % 5 == 0:
@@ -29,22 +28,3 @@ def fizzbuzzCalculator(n):
     else:
         return str(n)
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
